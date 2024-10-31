77d584c683a3d3eeb771246c1e91e0ad46bc7560
Antônio Magno <antoniomagnotrabalho@gmail.com>
2024-10-31 09:18:28 -0300

    Add files via upload

---
+++ 4_resgatando_com_where/app.js
@@ -0,0 +1,72 @@
+import express from "express";
+import { engine } from "express-handlebars";
+import mysql from "mysql";
+
+const app = express();
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+
+app.post("/post", (req, res) => {
+  const { title, pageqty } = req.body;
+  const insertNewBook = `INSERT INTO books (title,pageqty) VALUES ('${title}', '${pageqty}')`;
+  dbConnection.query(insertNewBook, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Operação sem Erros!");
+      res.redirect("/books");
+    }
+  });
+});
+
+app.get("/books", (req, res) => {
+  const selectBooks = `SELECT * FROM books`;
+  dbConnection.query(selectBooks, (err, data) => {
+    if (err) {
+      console.log("Erro na operação", err);
+    } else {
+      const books = data;
+      res.render("books", { books });
+      console.log(books);
+    }
+  });
+});
+
+app.get("/books/:id", (req, res) => {
+  const id = req.params.id;
+  const selectWhere = `SELECT * FROM books WHERE idbooks = ${id}`;
+  const book = dbConnection.query(selectWhere, (err, data) => {
+    const book = data[0];//data é uma lista, ai pra pegar o primeiro objeto usamos 0
+    
+    res.render("book", { book });
+  });
+});
+
+const dbConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "msql_node",
+});
+
+dbConnection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Banco conectado");
+    app.listen(3000);
+  }
+});
